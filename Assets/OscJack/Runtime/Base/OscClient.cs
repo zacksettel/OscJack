2f0440239f419ffa0296bac8ba69c87384d11bd0
Zack Konate <zz@coptox.com>
2020-03-19 15:53:40 -0400

    added connection checking to oscClient to avoid sending on an unopen socket

--- Assets/OscJack/Runtime/Base/OscClient.cs
+++ Assets/OscJack/Runtime/Base/OscClient.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Net;
 using System.Net.Sockets;
+using UnityEngine;
 
 namespace OscJack
 {
@@ -11,16 +12,38 @@ namespace OscJack
     {
         #region Object life cycle
 
+        [SerializeField]
+        private string targetIP;
+
+        [SerializeField]
+        private int targetPort;
+
         public OscClient(string destination, int port)
         {
-            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            targetIP = destination;
+            targetPort = port;
 
-            if (destination == "255.255.255.255")
+            //Debug.Log("***************************** OscClient");
+
+            clientSetup();
+        }
+
+        // makes sure that needed state has been defined
+        void clientSetup()
+        {
+            if (_encoder == null) _encoder = new OscPacketEncoder();
+
+            if (_socket != null) return;
+
+            //Debug.Log("******************************* clientSetup");
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            if (targetIP == "255.255.255.255")
                 _socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, true);
 
-            var dest = new IPEndPoint(IPAddress.Parse(destination), port);
+            var dest = new IPEndPoint(IPAddress.Parse(targetIP), targetPort);
             _socket.Connect(dest);
         }
+
 
         public void Dispose()
         {
@@ -34,74 +57,95 @@ namespace OscJack
 
         public void Send(string address)
         {
+            clientSetup();
+
+             
             _encoder.Clear();
             _encoder.Append(address);
             _encoder.Append(",");
-            _socket.Send(_encoder.Buffer, _encoder.Length, SocketFlags.None);
+            if (_socket.Connected) _socket.Send(_encoder.Buffer, _encoder.Length, SocketFlags.None);
         }
 
         public void Send(string address, int data)
         {
+            clientSetup();
             _encoder.Clear();
             _encoder.Append(address);
             _encoder.Append(",i");
             _encoder.Append(data);
-            _socket.Send(_encoder.Buffer, _encoder.Length, SocketFlags.None);
+            if (_socket.Connected) _socket.Send(_encoder.Buffer, _encoder.Length, SocketFlags.None);
+        }
+
+        public void Send(string address, string element1, int element2)
+        {
+            clientSetup();
+            _encoder.Clear();
+            _encoder.Append(address);
+            _encoder.Append(",si");
+            _encoder.Append(element1);
+            _encoder.Append(element2);
+            if (_socket.Connected) _socket.Send(_encoder.Buffer, _encoder.Length, SocketFlags.None);
         }
 
         public void Send(string address, int element1, int element2)
         {
+            clientSetup();
             _encoder.Clear();
             _encoder.Append(address);
             _encoder.Append(",ii");
             _encoder.Append(element1);
             _encoder.Append(element2);
-            _socket.Send(_encoder.Buffer, _encoder.Length, SocketFlags.None);
+            if (_socket.Connected) _socket.Send(_encoder.Buffer, _encoder.Length, SocketFlags.None);
         }
 
         public void Send(string address, int element1, int element2, int element3)
         {
+            clientSetup();
             _encoder.Clear();
             _encoder.Append(address);
             _encoder.Append(",iii");
             _encoder.Append(element1);
             _encoder.Append(element2);
             _encoder.Append(element3);
-            _socket.Send(_encoder.Buffer, _encoder.Length, SocketFlags.None);
+            if (_socket.Connected) _socket.Send(_encoder.Buffer, _encoder.Length, SocketFlags.None);
         }
 
         public void Send(string address, float data)
         {
+            clientSetup();
             _encoder.Clear();
             _encoder.Append(address);
             _encoder.Append(",f");
             _encoder.Append(data);
-            _socket.Send(_encoder.Buffer, _encoder.Length, SocketFlags.None);
+            if (_socket.Connected) _socket.Send(_encoder.Buffer, _encoder.Length, SocketFlags.None);
         }
 
         public void Send(string address, float element1, float element2)
         {
+            clientSetup();
             _encoder.Clear();
             _encoder.Append(address);
             _encoder.Append(",ff");
             _encoder.Append(element1);
             _encoder.Append(element2);
-            _socket.Send(_encoder.Buffer, _encoder.Length, SocketFlags.None);
+            if (_socket.Connected) _socket.Send(_encoder.Buffer, _encoder.Length, SocketFlags.None);
         }
 
         public void Send(string address, float element1, float element2, float element3)
         {
+            clientSetup();
             _encoder.Clear();
             _encoder.Append(address);
             _encoder.Append(",fff");
             _encoder.Append(element1);
             _encoder.Append(element2);
             _encoder.Append(element3);
-            _socket.Send(_encoder.Buffer, _encoder.Length, SocketFlags.None);
+            if (_socket.Connected) _socket.Send(_encoder.Buffer, _encoder.Length, SocketFlags.None);
         }
 
         public void Send(string address, float element1, float element2, float element3, float element4)
         {
+            clientSetup();
             _encoder.Clear();
             _encoder.Append(address);
             _encoder.Append(",ffff");
@@ -109,16 +153,17 @@ namespace OscJack
             _encoder.Append(element2);
             _encoder.Append(element3);
             _encoder.Append(element4);
-            _socket.Send(_encoder.Buffer, _encoder.Length, SocketFlags.None);
+            if (_socket.Connected) _socket.Send(_encoder.Buffer, _encoder.Length, SocketFlags.None);
         }
 
         public void Send(string address, string data)
         {
+            clientSetup();
             _encoder.Clear();
             _encoder.Append(address);
             _encoder.Append(",s");
             _encoder.Append(data);
-            _socket.Send(_encoder.Buffer, _encoder.Length, SocketFlags.None);
+            if (_socket.Connected) _socket.Send(_encoder.Buffer, _encoder.Length, SocketFlags.None);
         }
 
         #endregion
