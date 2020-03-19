6e0b666afafddf5329b7684fe74216701aa35a46
Zack Konate <zz@coptox.com>
2020-03-19 15:34:29 -0400

    testing branch

---
+++ Assets/soundTrigger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class soundTrigger : MonoBehaviour
+{
+    public AudioSource audiosrc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!audiosrc)
+            audiosrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            print("PLAY");
+            audiosrc.Play();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            print("STOP");
+            audiosrc.Stop();
+        }
+    }
+}
